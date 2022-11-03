9d30b56d2430911c9bc083dc4003791396d17069
jaydeep <jaydeep.r@peerbirs.com>
2022-11-03 10:53:00 +0530

    [Update]if conditions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shell_scripting
+
+This repository is made when I was learning about shell scripting
--- if-condition.sh
+++ if-condition.sh
@@ -3,8 +3,47 @@
 
 number=5
 
+# 1
 if test $number -eq 5
 then
-    echo "number is 5"
+    echo "number is equal to 5"
+
+fi
+
+
+# 2
+# We can write this instead of test
+if [ $number -eq 5 ]  # use "[ " instead of "test"
+then
+    echo "number is equal to 5"
+
+fi
+
+
+
+# 3
+# We can write this instead of test
+if [ $number -gt 3 ]  # use "[ " instead of "test"
+then
+    echo "number is equal to 5"
+
+fi
+
+
+
+
+# 4 - string comparison
+if [ "jayd" == "jayd" ]  # use "[ " instead of "test"
+then
+    echo "Both strings are equal"
+
+fi
+
+
+
+# 4 - string comparison without " --> TRUE
+if [ jayd == jayd ]
+then
+    echo "Both strings are equal"
 
 fi
